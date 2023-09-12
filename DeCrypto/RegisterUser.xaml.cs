d9fe445f64f01149ebf5016e2276d3985837355c
Dan <dansegura8863@gmail.com>
2023-09-12 14:20:25 -0600

    Creacion de ventanas Login y RegisterUser

--- DeCrypto/RegisterUser.xaml.cs
+++ DeCrypto/RegisterUser.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DeCrypto
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para RegisterUser.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegisterUser : Window
     {
-        public MainWindow()
+        public RegisterUser()
         {
             InitializeComponent();
         }
